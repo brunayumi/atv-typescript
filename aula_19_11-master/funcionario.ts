eeca7c3c0f4803ffd3739574c26502f1dc2fd269
yumi 3 <51716241+brunayumi@users.noreply.github.com>
2019-11-19 16:15:52 -0300

    aula typescript

---
+++ aula_19_11-master/funcionario.ts
@@ -0,0 +1,31 @@
+  namespace empresa{
+
+  export class Funcionario extends Pessoa{
+        private _codFuncionario:number;
+        private _setor:string;
+        private _banco:Banco;
+
+            public getCodFuncionario(){
+              return this._codFuncionario;
+            }
+            public setCodFuncionario(codigo:number){
+               this._codFuncionario = codigo;
+            }
+
+         public getSetor(){
+                return this._setor;
+             }
+
+            public setSetor(setor:string){
+            this._setor = setor;
+            }
+
+            public getBanco(){
+                return this._banco;
+             }
+
+            public setBanco(banco:Banco){
+            this._banco = banco;
+            }
+       }
+  }
---
+++ aula_19_11-master/implementa.ts
@@ -0,0 +1,47 @@
+
+///<reference path="pessoa.ts"/>
+///<reference path="cliente.ts"/>
+///<reference path="funcionario.ts"/>
+///<reference path="banco.ts"/>
+
+namespace empresa{
+     let banco = new Banco();
+     banco.setNome("Bradesco");
+     banco.setCodBanco(33);
+
+    let cliente = new Cliente();
+    cliente.setNome("João");
+    cliente. setCodCliente(1);
+    cliente.setCpf("000.000.000-00");
+    cliente. setIdade(30);
+    cliente. setCredito(2000);
+    cliente. setBanco(banco);
+
+
+    let cliente2 = new Cliente();
+    cliente2.setNome("Maria");
+    cliente2.setCodCliente(2);
+    cliente2.setCpf("111.000.000-11");
+    cliente2.setIdade(40);
+    cliente2. setCredito(3000);
+    cliente2. setBanco(banco);
+
+    document.getElementById("nome").textContent = cliente.getNome();
+    document.getElementById("idade").textContent = cliente.getIdade().toString();
+    document.getElementById("cpf").textContent = cliente.getCpf();
+    document.getElementById("codigo").textContent = cliente.getCodCliente().toString();
+    document.getElementById("credito").textContent = cliente.getCredito().toString();
+    document.getElementById("banco").textContent = cliente.getBanco().getNome();
+
+    banco. addCliente(cliente);
+    banco. addCliente(cliente2);
+
+    //trecho inserido para exemplificar a implentacao das tabelas
+    let tabela = document.getElementById("tabela");
+
+    let conteudo = " <tr> <th>Cliente</th> <th>Banco</th> </tr>";
+    banco.getClientes().forEach(element => {
+        conteudo += "<tr> <td>"+element.getNome()+"</td><td>"+ element.getBanco().getNome()+"</td></tr>";
+    });
+    tabela.innerHTML = conteudo;
+    }
